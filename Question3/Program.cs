06300d28b572704d642a6310d198d863db17830a
Adeyemi Olaopa <adeyemi.olaopa@sterling.ng>
2024-11-08 18:40:48 +0100

    Solution

--- Question3/Program.cs
+++ Question3/Program.cs
@@ -12,5 +12,25 @@ var courses = new List<Course>()
 
 var students = new List<Student>()
 {
-    new Student { Id = Guid.NewGuid() , Age = 3, Name = "Adeyemi", Gpa = 299, Courses = new List<Course>() { } }
+    new Student { Id = Guid.NewGuid() , Age = 3, Name = "Adeyemi", Gpa = 299, Courses = new List<Course>()
+    {
+         new Course { Id = Guid.NewGuid() , Credits = 1 , Name = "English"},
+        new Course { Id = Guid.NewGuid() , Credits = 5, Name = "Maths"}
+    }},
+
+    new Student { Id = Guid.NewGuid() , Age = 4, Name = "Oluseye", Gpa = 655, Courses = new List<Course>()
+    {
+         new Course { Id = Guid.NewGuid() , Credits = 1 , Name = "English"},
+        new Course { Id = Guid.NewGuid() , Credits = 5, Name = "Maths"}
+    }},
 };
+
+
+// sort students by gpa
+var sortedByGPA = students.OrderByDescending(x => x.Gpa);
+
+// sort students by age
+var sortedByAge = students.OrderByDescending(x => x.Age);
+
+// calculate average gpa
+var calculateAverageGPA = students.Sum(x => x.Gpa) / students.Count;
